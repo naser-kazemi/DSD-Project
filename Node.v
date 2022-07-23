f14036200f2f09bc9ce122faf61f9c4d8ba164ac
naser-kazemi <naserkazemi2002@gmail.com>
2022-07-23 18:01:14 +0430

    update Node.v

--- Node.v
+++ Node.v
@@ -5,12 +5,27 @@ module node(
     output out_x,
     output out_y
 );
+
+
     reg x, px, y, py;
     assign out_x = x;
     assign out_y = y;
 
+
+    reg vx, vy;
+
+
+
+    always @(in_x, in_y) begin: get_input
+            x = in_x;
+            y = in_y;
+        end
+
+
+    
     always @(posedge clk) begin: verlet_integration
-        
+        vx <= x - px;
+        vy <= y - py;
     end
 
 endmodule
