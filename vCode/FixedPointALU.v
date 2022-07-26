fca0d504f593e013ec7a6910d92669aa0976cb20
naser-kazemi <naserkazemi2002@gmail.com>
2022-07-26 11:44:30 +0430

    update FixedPointALU.v

--- vCode/FixedPointALU.v
+++ vCode/FixedPointALU.v
@@ -1,5 +1,5 @@
 module FixedPointALU # (
-    parameter Q = 12,
+    parameter Q = 20,
     parameter N = 32
 )
 (
@@ -24,25 +24,26 @@ assign sub = a - b;
 
 // multiplication
 wire [2*N-1:0]	f_result;		//	Multiplication by 2 values of N bits requires a 
-									//	register that is N+N = 2N deep
+                                //	register that is N+N = 2N deep
+                                
 wire [N-1:0]   multiplicand;
 wire [N-1:0]	multiplier;
 wire [N-1:0]    a_2cmp, b_2cmp;
 wire [N-2:0]    quantized_result,quantized_result_2cmp;
 
-assign a_2cmp = {a[N-1],{(N-1){1'b1}} - a[N-2:0]+ 1'b1};  //2's complement of a
-assign b_2cmp = {b[N-1],{(N-1){1'b1}} - b[N-2:0]+ 1'b1};  //2's complement of b
+assign a_2cmp = {~a[N-1],~a[N-2:0]+ 1'b1};  //2's complement of a {(N-1){1'b1}} - 
+assign b_2cmp = {~b[N-1],~b[N-2:0]+ 1'b1};  //2's complement of b  {(N-1){1'b1}} - 
 
 assign multiplicand = (a[N-1]) ? a_2cmp : a;              
 assign multiplier   = (b[N-1]) ? b_2cmp : b;
 
-assign mult[N-1] = a[N-1]^b[N-1];                      //Sign bit of output would be XOR or input sign bits
-assign f_result = multiplicand[N-2:0] * multiplier[N-2:0]; //We remove the sign bit for multiplication
-assign quantized_result = f_result[N-2+Q:Q];               //Quantization of output to required number of bits
-assign quantized_result_2cmp = {(N-1){1'b1}} - quantized_result[N-2:0] + 1'b1;  //2's complement of quantized_result
-assign mult[N-2:0] = (mult[N-1]) ? quantized_result_2cmp : quantized_result; //If the result is negative, we return a 2's complement representation 
-                                                         //of the output value
-assign overflow = (f_result[2*N-2:N-1+Q] > 0) ? 1'b1 : 1'b0;
+
+assign f_result = multiplicand[N-2:0] * multiplier[N-2:0];  //We remove the sign bit for multiplication
+
+assign mult[N-1] = a[N-1]^b[N-1];                     //Sign bit of output would be XOR or input sign bits
+assign quantized_result = f_result[N-2+Q:Q];              //Quantization of output to required number of bits
+assign quantized_result_2cmp = ~quantized_result[N-2:0] + 1'b1;  //2's complement of quantized_result  {(N-1){1'b1}} - 
+assign mult[N-2:0] = mult[N-1] ? quantized_result_2cmp : quantized_result; //If the result is negative, we return a 2's complement representation of the result
 
 
 
--- vCode/FixedPointALU_tb.v
+++ vCode/FixedPointALU_tb.v
@@ -2,23 +2,28 @@
 
 module FixedPointALU_tb ();
 
+parameter N = 32;
+parameter Q = 12;
 
-reg [31:0] x, y;
+
+reg [31:0] a, b;
 wire [31:0] result;
 reg [1:0] op;
 
 
-FixedPointALU fp_alu(x, y, op, result);
+FixedPointALU fp_alu(a, b, op, result);
 
 
 initial begin
 
-    x = 32'h00280000;
-    y = 32'h00366666;
+    a = 32'h003851ec;
+    b = 32'h00663d71;
     op = 2'b10;
 
     #2
     $display("result = %h", result);
+
+    
     
 end
 
