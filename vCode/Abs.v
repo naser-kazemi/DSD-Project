551e1f2dd675dee0f4c986049f2e8eb85305b836
naser-kazemi <naserkazemi2002@gmail.com>
2022-07-26 20:09:14 +0430

    modifiy Abs.v

--- vCode/Abs.v
+++ vCode/Abs.v
@@ -1,14 +1,16 @@
-module Abs (
+module Abs #( 
+    parameter N = 32
+)
+(
     input wire[31:0] number, 
-    output reg[31:0] abs_number
+    output [31:0] abs_number
 );
 
-assign number_2cmp = {~number[31],~number[30:0]+ 1'b1};
-    always @(*)begin
-        if (number[31]) begin
-            abs_number = number_2cmp;
-        end else begin
-            abs_number = number;
-        end
-    end
+
+wire [N - 1:0] number_2cmp;
+
+assign number_2cmp = {~number[N - 1],~number[N - 2:0]+ 1'b1};
+
+assign abs_number = number[N - 1] ? number_2cmp : number;
+
 endmodule
