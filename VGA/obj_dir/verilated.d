a6ba1c8294fa5cb0d5ca3b1f9c82caa9ffaa3162
naser-kazemi <naserkazemi2002@gmail.com>
2022-07-27 13:44:35 +0430

    test verilator

---
+++ VGA/obj_dir/verilated.d
@@ -0,0 +1,83 @@
+verilated.o: \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated.cpp \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilatedos.h \
+  /usr/local/include/pthread.h /usr/local/include/inttypes.h \
+  /usr/local/include/sys/types.h /usr/local/include/sys/appleapiopts.h \
+  /usr/local/include/sys/cdefs.h /usr/local/include/machine/types.h \
+  /usr/local/include/sys/_types.h /usr/local/include/machine/endian.h \
+  /usr/local/include/i386/endian.h /usr/local/include/sys/_endian.h \
+  /usr/local/include/libkern/_OSByteOrder.h \
+  /usr/local/include/libkern/i386/_OSByteOrder.h \
+  /usr/local/include/sys/_types/_u_char.h \
+  /usr/local/include/sys/_types/_u_short.h \
+  /usr/local/include/sys/_types/_u_int.h \
+  /usr/local/include/sys/_types/_caddr_t.h \
+  /usr/local/include/sys/_types/_dev_t.h \
+  /usr/local/include/sys/_types/_blkcnt_t.h \
+  /usr/local/include/sys/_types/_blksize_t.h \
+  /usr/local/include/sys/_types/_gid_t.h \
+  /usr/local/include/sys/_types/_in_addr_t.h \
+  /usr/local/include/sys/_types/_in_port_t.h \
+  /usr/local/include/sys/_types/_ino_t.h \
+  /usr/local/include/sys/_types/_ino64_t.h \
+  /usr/local/include/sys/_types/_key_t.h \
+  /usr/local/include/sys/_types/_mode_t.h \
+  /usr/local/include/sys/_types/_nlink_t.h \
+  /usr/local/include/sys/_types/_id_t.h \
+  /usr/local/include/sys/_types/_pid_t.h \
+  /usr/local/include/sys/_types/_off_t.h \
+  /usr/local/include/sys/_types/_uid_t.h \
+  /usr/local/include/sys/_types/_clock_t.h \
+  /usr/local/include/sys/_types/_size_t.h \
+  /usr/local/include/machine/_types.h \
+  /usr/local/include/sys/_types/_ssize_t.h \
+  /usr/local/include/sys/_types/_time_t.h \
+  /usr/local/include/sys/_types/_useconds_t.h \
+  /usr/local/include/sys/_types/_suseconds_t.h \
+  /usr/local/include/sys/_types/_rsize_t.h \
+  /usr/local/include/sys/_types/_errno_t.h \
+  /usr/local/include/sys/_types/_fd_def.h \
+  /usr/local/include/Availability.h \
+  /usr/local/include/sys/_types/_fd_setsize.h \
+  /usr/local/include/sys/_types/_fd_set.h \
+  /usr/local/include/sys/_types/_fd_clr.h \
+  /usr/local/include/sys/_types/_fd_zero.h \
+  /usr/local/include/sys/_types/_fd_isset.h \
+  /usr/local/include/sys/_types/_fd_copy.h \
+  /usr/local/include/sys/_pthread/_pthread_attr_t.h \
+  /usr/local/include/sys/_pthread/_pthread_types.h \
+  /usr/local/include/sys/_pthread/_pthread_cond_t.h \
+  /usr/local/include/sys/_pthread/_pthread_condattr_t.h \
+  /usr/local/include/sys/_pthread/_pthread_mutex_t.h \
+  /usr/local/include/sys/_pthread/_pthread_mutexattr_t.h \
+  /usr/local/include/sys/_pthread/_pthread_once_t.h \
+  /usr/local/include/sys/_pthread/_pthread_rwlock_t.h \
+  /usr/local/include/sys/_pthread/_pthread_rwlockattr_t.h \
+  /usr/local/include/sys/_pthread/_pthread_t.h \
+  /usr/local/include/sys/_pthread/_pthread_key_t.h \
+  /usr/local/include/sys/_types/_fsblkcnt_t.h \
+  /usr/local/include/sys/_types/_fsfilcnt_t.h \
+  /usr/local/include/unistd.h /usr/local/include/_types.h \
+  /usr/local/include/sys/unistd.h \
+  /usr/local/include/sys/_types/_posix_vdisable.h \
+  /usr/local/include/sys/_types/_seek_set.h \
+  /usr/local/include/_types/_uint64_t.h \
+  /usr/local/include/_types/_uint32_t.h \
+  /usr/local/include/sys/_types/_intptr_t.h \
+  /usr/local/include/sys/_types/_null.h /usr/local/include/_ctermid.h \
+  /usr/local/include/sys/select.h \
+  /usr/local/include/sys/_types/_timespec.h \
+  /usr/local/include/sys/_types/_timeval.h \
+  /usr/local/include/sys/_types/_sigset_t.h \
+  /usr/local/include/sys/_select.h \
+  /usr/local/include/sys/_types/_uuid_t.h \
+  /usr/local/include/gethostuuid.h \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated_imp.h \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated.h \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated_config.h \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated_types.h \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated_funcs.h \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated_syms.h \
+  /usr/local/Cellar/verilator/4.224/share/verilator/include/verilated_sym_props.h \
+  /usr/local/include/sys/stat.h /usr/local/include/sys/_types/_s_ifmt.h \
+  /usr/local/include/sys/_types/_filesec_t.h
