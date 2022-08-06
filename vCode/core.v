0c10fe95795040e06a8e2e9d6852e8fab40d2375
Arash <arashyadegari0402@gmail.com>
2022-08-06 22:32:38 +0430

    change to memory base architecture

--- vCode/core.v
+++ vCode/core.v
@@ -1,153 +1,80 @@
-`include "node.v"
-`include "circular_shift.v"
-
+`include "core_control_unit.v"
+`include "verlet_integration_unit.v"
+`include "input_selector.v"
+`include "mouse_distance_unit.v"
 
 module core #(
     parameter node_contains = 5,
-    parameter core_id = 1
+    parameter core_id = 1,
+    parameter width = 32
 ) (
-  input wire clk, 
-  input wire reset,
-  input wire [31:0] prev_core_last_x,
-  input wire [31:0] prev_core_last_y,
-  input wire [31:0] next_core_first_x,
-  input wire [31:0] next_core_first_y,
-  input wire [31:0] x_mouse,
-  input wire [31:0] y_mouse,
-  input wire is_last,
-  output wire[node_contains * 32 -1:0] nodes_x,
-  output wire[node_contains * 32 -1:0] nodes_y
+    input wire clk, 
+    input wire reset,
+    input wire [width-1:0] x_mouse,
+    input wire [width-1:0] y_mouse,
+    input wire [width-1:0] enforce_upper_x;
+    input wire [width-1:0] enforce_upper_y;
+    input wire [width-1:0] enforce_lower_x;
+    input wire [width-1:0] enforce_lower_y;
+    output wire [(width-1) * node_contains - 1:0] all_nodes_x_position,
+    output wire [(width-1) * node_contains - 1:0] all_nodes_y_position
 );
 
-wire [node_contains :0] control_signal;
-reg [node_contains:0] control_signal_reg;
-wire [node_contains:0] next_control_signal;
 
-assign control_signal = control_signal_reg; 
-circular_shift #(node_contains + 1) cs(control_signal, next_control_signal);
+reg [4*(width)-1:0] ram[node_contains-1:0];
+wire [width-1:0] verlet_cnt_sig;
+wire [width-1:0] data_in_address;
 
-wire [31:0] x_pos[node_contains - 1:0]; 
-wire [31:0] y_pos[node_contains - 1:0];
-wire [31:0] new_x_pos[node_contains - 1:0];
-wire [31:0] new_y_pos[node_contains - 1:0];
-wire [node_contains -1:0] is_last_node_of_last_core;
-
+core_control_unit cu(
+    .clk(clk),
+    .reset(reset),
+    .verlet_cnt_sig(verlet_cnt_sig),
+    .ram_data_in_address(data_in_address)
+);
 
 
+wire [width-1:0] x_position, y_position,
+ pre_x_position, pre_y_position, fixed_x, fixed_y; 
 
-genvar i;
-generate
-    for(i = 0; i < node_contains; i = i + 1) begin
-        assign is_last_node_of_last_core[i] = is_last && (node_contains - 1 == i); 
-        assign nodes_x[(i + 1) * 32 - 1: i * 32] = x_pos[i];
-        assign nodes_y[(i + 1) * 32 - 1: i * 32] = y_pos[i];
-        
-        node #(
-            (i + 1) + (core_id -1) * 5
-        ) node(
-            clk,
-            reset,
-            control_signal[i],
-            control_signal[node_contains],
-            new_x_pos[i],
-            new_y_pos[i],
-            x_mouse, 
-            y_mouse,
-            x_pos[i],
-            y_pos[i]
-            );
+ wire [node_contains*4*width -1:0] ram_flatted;
 
-        if (core_id == 1) begin
-            if (i == 0) begin
-                assign new_x_pos[0] = x_pos[0];
-                assign new_y_pos[0] = y_pos[0];
-            end
-            else if (i < node_contains - 1) begin
-                enforce_constraint #(
-                    (i + 1) + (core_id -1) * 5
-                ) e(
-                    x_pos[i - 1],
-                    y_pos[i - 1],
-                    x_pos[i],
-                    y_pos[i],
-                    x_pos[i + 1],
-                    y_pos[i + 1],
-                    is_last_node_of_last_core[i],
-                    new_x_pos[i],
-                    new_y_pos[i]
-                );
-            end
-        end
-
-        else begin
-            if (i == 0) begin
-                enforce_constraint #(
-                        (i + 1) + (core_id -1) * 5
-                    ) e(
-                        prev_core_last_x,
-                        prev_core_last_y,
-                        x_pos[i],
-                        y_pos[i],
-                        x_pos[i + 1],
-                        y_pos[i + 1],
-                        is_last_node_of_last_core[i],
-                        new_x_pos[i],
-                        new_y_pos[i]
-                );
-            end
-            else if (i < node_contains - 1) begin
-                enforce_constraint #(
-                    (i + 1) + (core_id -1) * 5
-                ) e(
-                    x_pos[i - 1],
-                    y_pos[i - 1],
-                    x_pos[i],
-                    y_pos[i],
-                    x_pos[i + 1],
-                    y_pos[i + 1],
-                    is_last_node_of_last_core[i],
-                    new_x_pos[i],
-                    new_y_pos[i]
-                );
-            end
-        end
-        
-
-        if (i == node_contains - 1) begin
-            enforce_constraint #(
-                (i + 1) + (core_id -1) * 5
-            ) e(
-                x_pos[i - 1],
-                y_pos[i - 1],
-                x_pos[i],
-                y_pos[i],
-                next_core_first_x,
-                next_core_first_y,
-                is_last_node_of_last_core[i],
-                new_x_pos[i],
-                new_y_pos[i]
-                );
-        end
-    end
-endgenerate
+ genvar i;
+  generate for (i = 0; i < node_contains; i = i+1) begin
+    assign ram_flatted[4*width*i +: 4*width] = ram[i]; 
+  end endgenerate
 
 
-// integer j;
-always @(posedge clk) begin
-    if(!reset) begin
-    control_signal_reg <= next_control_signal;
+input_selector is(
+    .ram_flatted(ram_flatted),
+    .selector(verlet_cnt_sig),
+    .pre_x_position(pre_x_position),
+    .pre_y_position(pre_y_position),
+    .x_position(x_position),
+    .y_position(y_position)
+);
 
-    end else begin
-        control_signal_reg <= 1;
-    end
+verlet_integration_unit viu(
+    .x_position(x_position), 
+    .y_position(y_position),
+    .pre_x_position(pre_x_position),
+    .pre_y_position(pre_y_position),
+    .fixed_x(fixed_x),
+    .fixed_y(fixed_y)
+); 
+
+wire [width-1:0] pre_x_mouse_affected;
+mouse_distance_unit mdu(
+    .x_pos(x_position),
+    .y_pos(y_position),
+    .x_mouse(x_mouse),
+    .y_mouse(y_mouse),
+    .x_affected(pre_x_mouse_affected)
+);
 
 
-
+always @(posedge clk ) begin: write_new_verlete_integrated_value
+    ram[data_in_address] <= {y_position, pre_x_mouse_affected, fixed_y, fixed_x};
 end
-
-
-
-
 
 
 endmodule
